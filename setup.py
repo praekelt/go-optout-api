eb9d57446b3b45375bc0678691bc2cb7aa28dbfb
Simon Cross <hodgestar@gmail.com>
2015-05-11 09:46:25 +0200

    Initial skeleton.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="go-optouts",
+    version="0.0.1a",
+    url='http://github.com/praekelt/go-optouts-api',
+    license='BSD',
+    description="An API for managing Vumi Go opt outs.",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.org',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
