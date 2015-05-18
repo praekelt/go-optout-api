ace75ffd026e8225bac8e4728f573c4a41316983
Trevor <trevor@projectcodex.co>
2015-05-18 11:32:14 +0200

    issue-5-create-hello-world-klein-application updated

--- go_optouts/__init__.py
+++
@@ -1,5 +0,0 @@
-"""An API for managing Vumi Go opt outs."""
-
-__version__ = "0.0.1a"
-
-__all__ = []
--- go_optouts/run.py
+++
@@ -1,5 +0,0 @@
-import app
- 
-# TODO use twistd instead
-# http://klein.readthedocs.org/en/latest/introduction/2-twistdtap.html
-app.run('localhost', 8080)
