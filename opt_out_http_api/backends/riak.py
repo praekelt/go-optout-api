c88e152e45cf8436c1fdca14cef102fbd86d175f
Simon Cross <hodgestar@gmail.com>
2015-06-15 18:47:38 +0200

    Riak backend with tests.

--- opt_out_http_api/backends/riak.py
+++ opt_out_http_api/backends/riak.py
@@ -1,5 +1,86 @@
-"""
-This is where the "real" backend implementation will live.
+""" Riak opt out backend. """
 
-Not worry about it for the moment.
-"""
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+from go.vumitools.opt_out import OptOutStore, OptOut
+
+
+class RiakOptOutBackend(object):
+    """ Riak opt out backend.
+
+    :type riak_manager:
+        `vumi.persist.txriak_manager.TxRiakManager`
+    :param riak_manager:
+        A Riak manager for the opt out stores.
+    """
+
+    # TODO: this should declare that it implements the
+    #       opt out backend interface
+
+    def __init__(self, riak_manager):
+        self.riak_manager = riak_manager
+
+    def get_opt_out_collection(self, owner_id):
+        """ Return the opt out collection for the specified owner.
+
+        :param str owner_id:
+            The id of the owner of the opt out store.
+        """
+        opt_out_store = OptOutStore(self.riak_manager, owner_id)
+        return RiakOptOutCollection(opt_out_store)
+
+
+class RiakOptOutCollection(object):
+    """ Riak opt out collection for a particular opt out store.
+
+    :type opt_out_store:
+        `go.vumitools.opt_out.models.OptOutStore`
+    :param opt_out_store:
+        The opt out store to provide access to.
+    """
+
+    # TODO: this should declare that it implements the
+    #       opt out collection interface
+
+    def __init__(self, opt_out_store):
+        self.store = opt_out_store
+
+    @classmethod
+    def _pick_fields(cls, data, keys):
+        """
+        Return a sub-dictionary of all the items from ``data`` whose
+        keys are listed in ``keys``.
+        """
+        return dict((k, data[k]) for k in keys if k in data)
+
+    @classmethod
+    def _opt_out_to_dict(cls, opt_out):
+        """
+        Return a sub-dictionary of the items from ``data`` that are valid
+        contact fields.
+        """
+        return cls._pick_fields(
+            opt_out.get_data(), OptOut.field_descriptors.keys())
+
+    @inlineCallbacks
+    def get_opt_out(self, addresstype, address):
+        opt_out = yield self.store.get_opt_out(addresstype, address)
+        if opt_out is None:
+            returnValue(None)
+        returnValue(self._opt_out_to_dict(opt_out))
+
+    @inlineCallbacks
+    def save_opt_out(self, addresstype, address):
+        opt_out = yield self.store.new_opt_out(
+            addresstype, address, message={
+                'message_id': None,  # TODO: fix opt out store
+            })
+        returnValue(self._opt_out_to_dict(opt_out))
+
+    @inlineCallbacks
+    def delete_opt_out(self, addresstype, address):
+        opt_out = yield self.store.get_opt_out(addresstype, address)
+        if opt_out is None:
+            returnValue(None)
+        yield opt_out.delete()
+        returnValue(opt_out)
