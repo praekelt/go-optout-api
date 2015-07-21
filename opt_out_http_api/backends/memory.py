36b5f38ccb21ee345085a3a0e8699db5fccade23
Simon Cross <hodgestar@gmail.com>
2015-07-21 13:55:28 +0200

    Move riak store into opt_out_http_api.store.

--- opt_out_http_api/backends/memory.py
+++
@@ -1,9 +0,0 @@
-"""
-This is where we will put the in-memory opt out store that we
-will use for testing and simple demos.
-
-Essentially it's the same as the implementation that exists in
-api_methods right now (i.e. a list of opt outs in memory), but
-it will conform to the interface in backends.interface and
-perhaps be a little nicer.
-"""
